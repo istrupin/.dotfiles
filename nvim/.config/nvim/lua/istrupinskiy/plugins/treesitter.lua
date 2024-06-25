70517a25fcd177682863d01c60d2f6964805564c
Igor Strupinskiy <igor.strupinskiy@gmail.com>
2024-06-24 17:40:48 -0700

    fix treesitter and change some keys

--- nvim/.config/nvim/lua/istrupinskiy/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/istrupinskiy/plugins/treesitter.lua
@@ -23,6 +23,10 @@ return {
             additional_vim_regex_highlighting = false,
         },
 
-    }
+    },
+    config = function(_, opts)
+        local configs = require("nvim-treesitter.configs")
+        configs.setup(opts)
+    end
 
 }
