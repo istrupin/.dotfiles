1b49cbe915a92089100e9d8bd1bf35c93ac352e0
Igor Strupinskiy <igor.strupinskiy@gmail.com>
2024-07-30 10:33:02 -0700

    mini.ai and treesitter text objects, also enable incremental select

--- nvim/.config/nvim/lua/istrupinskiy/plugins/mini-nvim.lua
+++ nvim/.config/nvim/lua/istrupinskiy/plugins/mini-nvim.lua
@@ -1,10 +1,26 @@
-return { 
+return {
     'echasnovski/mini.nvim',
+    -- dependencies are for function definition querying in mini.ai
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        'nvim-treesitter/nvim-treesitter',
+    },
+    event = "VeryLazy",
     version = false,
     config = function(_, opts)
         require('mini.bracketed').setup({
-            undo       = { suffix = '', options = {} },
+            undo = { suffix = '', options = {} },
+        })
+        local ai = require("mini.ai")
+        ai.setup({
+            custom_textobjects = {
+                -- for function definition.  This way, 'f' is function call, 'F' us function definition
+                F = ai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }),
+                o = ai.gen_spec.treesitter({ -- code block
+                    a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+                    i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+                }),
+            }
         })
     end
 }
-
